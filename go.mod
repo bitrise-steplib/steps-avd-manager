c175c0975ac38fdaef32051e39e8e1d240f7df64
Isaac Halvorson <hello@hisaac.net>
2022-07-14 14:56:37 -0500

    WIP

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/bitrise-io/go-android v0.0.0-20210527143215-3ad22ad02e2e
 	github.com/bitrise-io/go-steputils v1.0.2
 	github.com/bitrise-io/go-utils v1.0.2
+	github.com/bitrise-io/go-utils/v2 v2.0.0-alpha.7.0.20220714185021-dfb5eaedc3c2
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 )
---
+++ vendor/github.com/bitrise-io/go-utils/v2/system/cpu.go
@@ -0,0 +1,38 @@
+package system
+
+import (
+	"strings"
+
+	"golang.org/x/sys/unix"
+)
+
+type cpu struct{}
+
+// CPU is a singleton that returns information about the CPU
+var CPU cpu
+
+// Architecture returns the CPU's architecture name as a string
+func (cpu) Architecture() (string, error) {
+	var utsname unix.Utsname
+	err := unix.Uname(&utsname)
+	if err != nil {
+		return "", err
+	}
+
+	architecture := string(utsname.Machine[:])
+	architecture = strings.Trim(architecture, "\x00")
+	return architecture, nil
+}
+
+// IsARM returns true if the CPU's architecture is ARM
+//
+// Specifically, it returns true if the architecture's name contains the string 'arm'
+func (cpu) IsARM() (bool, error) {
+	cpuType, err := CPU.Architecture()
+
+	if err != nil {
+		return false, err
+	}
+
+	return strings.Contains(cpuType, "arm"), nil
+}
